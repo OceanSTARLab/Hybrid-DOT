e6f583ddc7d8149fd9bdc365e949c5d884e77dc3
OceanSTAR Lab <116273999+OceanSTARLab@users.noreply.github.com>
2025-05-17 14:45:39 +0800

    Add files via upload

---
+++ Hybrid-DOT/guided_diffusion/measurements.py
@@ -0,0 +1,109 @@
+'''This module handles task-dependent operations (A) and noises (n) to simulate a measurement y=Ax+n.'''
+
+from abc import ABC, abstractmethod
+from functools import partial
+from torch.nn import functional as F
+from torchvision import torch
+from util.resizer import Resizer
+
+
+# =================
+# Operation classes
+# =================
+
+__OPERATOR__ = {}
+
+
+def register_operator(name: str):
+    def wrapper(cls):
+        if __OPERATOR__.get(name, None):
+            raise NameError(f"Name {name} is already registered!")
+        __OPERATOR__[name] = cls
+        return cls
+    return wrapper
+
+
+def get_operator(name: str, **kwargs):
+    if __OPERATOR__.get(name, None) is None:
+        raise NameError(f"Name {name} is not defined.")
+    return __OPERATOR__[name](**kwargs)
+
+
+class LinearOperator(ABC):
+    @abstractmethod
+    def forward(self, data, **kwargs):
+        # calculate A * X
+        pass
+
+    @abstractmethod
+    def transpose(self, data, **kwargs):
+        # calculate A^T * X
+        pass
+    
+    def ortho_project(self, data, **kwargs):
+        # calculate (I - A^T * A)X
+        return data - self.transpose(self.forward(data, **kwargs), **kwargs)
+
+    def project(self, data, measurement, **kwargs):
+        # calculate (I - A^T * A)Y - AX
+        return self.ortho_project(measurement, **kwargs) - self.forward(data, **kwargs)
+
+
+@register_operator(name='super_resolution')
+class SuperResolutionOperator(LinearOperator):
+    def __init__(self, in_shape, sr_factor, device):
+        self.device = device
+        self.up_sample = partial(F.interpolate, scale_factor=sr_factor)
+        self.down_sample = Resizer(in_shape, 1/sr_factor).to(device)
+
+    def forward(self, data, **kwargs):
+        return self.down_sample(data)
+
+    def transpose(self, data, **kwargs):
+        return self.up_sample(data)
+
+    def project(self, data, measurement, **kwargs):
+        return data - self.transpose(self.forward(data)) + self.transpose(measurement)
+
+
+# =============
+# Noise classes
+# =============
+
+
+__NOISE__ = {}
+
+
+def register_noise(name: str):
+    def wrapper(cls):
+        if __NOISE__.get(name, None):
+            raise NameError(f"Name {name} is already defined!")
+        __NOISE__[name] = cls
+        return cls
+    return wrapper
+
+
+def get_noise(name: str, **kwargs):
+    if __NOISE__.get(name, None) is None:
+        raise NameError(f"Name {name} is not defined.")
+    noiser = __NOISE__[name](**kwargs)
+    noiser.__name__ = name
+    return noiser
+
+
+class Noise(ABC):
+    def __call__(self, data):
+        return self.forward(data)
+    
+    @abstractmethod
+    def forward(self, data):
+        pass
+
+
+@register_noise(name='gaussian')
+class GaussianNoise(Noise):
+    def __init__(self, sigma):
+        self.sigma = sigma
+    
+    def forward(self, data):
+        return data + torch.randn_like(data, device=data.device) * self.sigma / 40
